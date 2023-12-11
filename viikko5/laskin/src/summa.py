471012c19a58624fd4c8e22fbfbcfeaaa64d2c71
Johanna Reinikainen <johanna.t.reinikainen@gmail.com>
2023-12-11 17:04:27 +0200

    Tehtävä 6.2

---
+++ viikko5/laskin/src/summa.py
@@ -0,0 +1,7 @@
+class Summa:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+
+    def suorita(self):
+        pass
