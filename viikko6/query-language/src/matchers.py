175186e2e5fc2b1757ab06fde40296689470796c
Johanna Reinikainen <johanna.t.reinikainen@gmail.com>
2023-12-11 17:08:12 +0200

    Tehtävä 6.3

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -51,3 +51,15 @@ class HasFewerThan:
 class All:
     def test(self, *args):
         return True
+
+
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+
+        return False
